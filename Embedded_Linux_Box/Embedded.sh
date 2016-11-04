55eac4f84d1ea48e7dfefafd7821c546a755ef51
Robert R. Pierce <pierril843@gmail.com>
2016-11-04 13:14:41 -0400

    updated sripts to check if called with sudo and add stream command

--- Embedded_Linux_Box/Embedded.sh
+++ Embedded_Linux_Box/Embedded.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+HOSTIP="$(hostname -I)"
+PORT="8070"
+
+
 echo "Embedded Init Starting..........."
 chmod 777 /dev/ttyUSB0
+chmod 777 /dev/video0
+sleep 2s
 echo "you may now run the Embedded Platform"
+echo "attempting to stream over $HOSTIP on Port $PORT"
+mjpg_streamer -i "input_uvc.so -d /dev/video0 -r VGA -y -f 50 " -o "output_http.so -p $PORT -w /home/mate/mjpg-streamer-code-182/mjpg-streamer/www"
--- Supervisor_Platform/Supervisor.sh
+++ Supervisor_Platform/Supervisor.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+HOSTIP="192.168.0.108"
+
 echo "Supervisor Init Starting..........."
 chmod 777 /dev/input/js0
 chmod 777 /dev/ttyUSB0
 sleep 2s
 echo "you may now run the Supervisor Platform"
+#echo "attempting to stream from $HOSTIP on Port $PORT"
